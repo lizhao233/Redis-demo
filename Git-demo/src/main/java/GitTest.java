1ae4b0ac71dd3873e4c6916660f1f0145ded75b4
atguigu <atguigu@edu>
2020-09-01 16:57:25 +0800

    创建一个测试类 GitTest 和pom.xml

---
+++ Git-demo/src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+    }
+}
