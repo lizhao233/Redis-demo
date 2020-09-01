a98415e08d5b29514db5f2c1bf0fe2f9c738523c
atguigu <atguigu@edu>
2020-09-01 17:02:41 +0800

    master 修改文件 测试冲突

--- Git-demo/src/main/java/GitTest.java
+++ Git-demo/src/main/java/GitTest.java
@@ -2,7 +2,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("123");//edit by hotfix
         System.out.println("123");
-        System.out.println("123");
+        System.out.println("123");//edit by master
         System.out.println("123");
         System.out.println("123");
         System.out.println("123");
